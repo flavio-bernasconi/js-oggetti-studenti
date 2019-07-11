e8b963f19f5cf855d3b23e8e4b5b451d8d5f90da
flavio-bernasconi <info@flaviobernasconi.it>
2019-07-11 10:38:06 +0200

    new commit

--- main.js
+++ main.js
@@ -1,30 +1,22 @@
 console.log("PRIMA PARTE");
 // creo il primo oggetto studenteUno
 var studenteUno = {
-  nome : "Gon",
-  cognome : "Fricks",
+  nome : "flavio",
+  cognome : "bernasconi",
   eta : 12
 }
 //x corrisponde al valore della chiave
-for (var x in studenteUno) {
-  console.log(studenteUno[x]);
+for (var valoreKey in studenteUno) {
+  console.log(studenteUno[valoreKey]);
 }
 ////////////
 console.log("SECONDA PARTE");
-//creo altri oggetti stundente
-var studenteDue = {
-  nome : "edward",
-  cognome : "elric",
-  eta : 14
-}
-var studenteTre = {
-  nome : "ken",
-  cognome : "kaneki",
-  eta : 17
-}
-
 //creo un array che contiene gli oggetti stundente
-var studenti = [studenteUno,studenteDue,studenteTre];
+var studenti = [
+  { nome : "marco ",cognome : "sassi",eta : 14 },
+  { nome : "matteo",cognome : "rossi", eta : 17 },
+  { nome : "mirco",cognome : "neri", eta : 17 }
+]
 //inizializzo un ciclo for della lunghezza dell array stundenti
 //dove stampo il valore della chiave nome e cognome per ognuno di loro
 for (var i = 0; i < studenti.length; i++) {
@@ -35,21 +27,13 @@ console.log("TERZA  PARTE");
 //l utente inserisce 3 prompt che valorizza una variabile
 var nomeInserito = prompt("inserisci nuovo nome");
 var cognomeInserito = prompt("inserisci nuovo cognome");
-var etaInserita = prompt("inserisci nuovo eta");
+var etaInserita = parseInt(prompt("inserisci nuovo eta"));
 
 //creo un oggetto dove le chiavi hanno un valore definito da variabili
-var newStudente = {
-  nome : nomeInserito,
-  cognome : cognomeInserito,
-  eta : etaInserita
-}
-//oppure lascio l oggetto vuoto (newStudente) e definisco
-//qui le sue proprieta
-// newStudente.nome = nomeInserito;
-// newStudente.cognome = cognomeInserito;
-// newStudente.eta = etaInserita;
 
-console.log(newStudente);
+studenti.push({nome : nomeInserito ,cognome : cognomeInserito , eta : etaInserita });
+console.log(studenti);
+
 
 
 
